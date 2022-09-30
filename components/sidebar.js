043e994c2b0107695ef85fa01b13442ced89b672
MSBivens <mikebiv95@gmail.com>
2022-09-30 01:05:34 -0500

    drafted

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,13 +1,29 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function Layout({ children }) {
+export default function Sidebar({ children }) {
   const router = useRouter();
 
   const menuItems = [
     {
       href: "/",
       title: "Home",
+    },
+    {
+      href: "/notifications",
+      title: "Notifications",
+    },
+    {
+      href: "/messages",
+      title: "Messages",
+    },
+    {
+      href: "/bookmarks",
+      title: "Bookmarks",
+    },
+    {
+      href: "/lists",
+      title: "Lists",
     },
     {
       href: "/subscriptions",
@@ -17,32 +33,35 @@ export default function Layout({ children }) {
       href: "/profile",
       title: "My Profile",
     },
+    // This needs to be a button
+    {
+      href: "/newpost",
+      title: "New Post",
+    },
   ];
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex flex-col md:flex-row flex-1">
-        <aside className="bg-fuchsia-100 w-full md:w-60">
-          <nav>
-            <ul>
-              {menuItems.map(({ href, title }) => (
-                <li className="m-2" key={title}>
-                  <Link href={href}>
-                    <a
-                      className={`flex p-2 bg-fuchsia-200 rounded hover:bg-fuchsia-400 cursor-pointer ${
-                        router.asPath === href && "bg-fuchsia-600 text-white"
-                      }`}
-                    >
-                      {title}
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </aside>
-        <main className="flex-1">{children}</main>
-      </div>
+    <div className="flex flex-col md:flex-row flex-1">
+      <aside className="bg-fuchsia-100 w-full md:w-60">
+        <nav>
+          <ul>
+            {menuItems.map(({ href, title }) => (
+              <li className="m-2" key={title}>
+                <Link href={href}>
+                  <a
+                    className={`flex p-2 rounded hover:underline cursor-pointer ${
+                      router.asPath === href && "text-grey"
+                    }`}
+                  >
+                    {title}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </aside>
+      <main className="flex-1">{children}</main>
     </div>
   );
 }
